1c416904130e11c1919dfcfdbc0de504f4f7a133
junexi0828 <junexi0828@gmail.com>
2025-06-02 17:23:19 +0900

    feat: 데이터베이스 스키마 동기화 및 백엔드 설정 완료

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -1,45 +1,36 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const db = require('./sequelize');
+const { DataTypes } = require('sequelize');
 
-const Review = db.define('Review', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'Users',
-      key: 'id'
-    }
-  },
-  restaurantId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'Restaurants',
-      key: 'id'
-    }
-  },
-  rating: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    validate: {
-      min: 1,
-      max: 5
-    }
-  },
-  comment: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  images: {
-    type: DataTypes.JSON
-  }
-}, {
-  timestamps: true
-});
+module.exports = (sequelize) => {
+  const Review = sequelize.define('Review', {
+    review_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    restaurant_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  }, {
+    tableName: 'reviews',
+    timestamps: false,
+  });
 
-module.exports = Review; 
+  return Review;
+}; 
