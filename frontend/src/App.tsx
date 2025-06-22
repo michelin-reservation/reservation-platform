04686482e919d003a154ea7c8d48bf539fbd20c9
junexi0828 <junexi0828@gmail.com>
2025-06-23 05:05:00 +0900

    bwh 브랜치 프론트엔드 변경점만 안전 병합

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
+import ScrollToTop from "./components/ScrollToTop";
 import HomePage from './pages/HomePage';
 import RestaurantPage from './pages/RestaurantPage';
 import ConciergeServices from './pages/ConciergeServices';
@@ -16,11 +17,24 @@ import MenuManagement from './pages/business/MenuManagement';
 import NoticeManagement from './pages/business/NoticeManagement';
 import RestaurantSettings from './pages/business/RestaurantSettings';
 import HelpSupport from './pages/business/HelpSupport';
+import AboutEIE from './pages/footer/AboutEIE';
+import BrandStory from './pages/footer/BrandStory';
+import Partnership from './pages/footer/Partnership';
+import PrivacyPolicy from './pages/footer/PrivacyPolicy';
+import TermsOfUse from './pages/footer/TermsOfUse';
+import TermsOfPayment from './pages/footer/TermsOfPayment';
+import CookiePolicy from './pages/footer/CookiePolicy';
+import FAQPage from './pages/footer/FAQPage';
+import ServiceGuide from './pages/footer/ServiceGuide';
+import MembershipPage from './pages/footer/MembershipPage';
+import VIPServices from './pages/footer/VIPServices';
+import Notice from './pages/footer/Notice';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
+      <ScrollToTop />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/restaurants" element={<RestaurantsPage />} />
@@ -37,6 +51,18 @@ function App() {
           <Route path="/business/notices" element={<NoticeManagement />} />
           <Route path="/business/settings" element={<RestaurantSettings />} />
           <Route path="/business/help" element={<HelpSupport />} />
+          <Route path="/footer/about-us/aboutEIEConcierge" element={<AboutEIE />} />
+          <Route path="/footer/about-us/brandstory" element={<BrandStory />} />
+          <Route path="/footer/about-us/partnership" element={<Partnership/>} />
+          <Route path="/footer/terms-and-policy/terms-of-use" element={<TermsOfUse />} />
+          <Route path="/footer/terms-and-policy/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/footer/terms-and-policy/terms-of-payment" element={<TermsOfPayment />} />
+          <Route path="/footer/terms-and-policy/cookie-policy" element={<CookiePolicy />} />
+          <Route path="/footer/customer-support/notice" element={<Notice />} />
+          <Route path="/footer/customer-support/faq" element={<FAQPage />} />
+          <Route path="/footer/customer-support/service-guide" element={<ServiceGuide />} />
+          <Route path="/footer/service-contents/membership" element={<MembershipPage />} />
+          <Route path="/footer/service-contents/vip-services" element={<VIPServices />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
